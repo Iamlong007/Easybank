720356587fd614792be72114dac134a7e0cab25a
Iamlong007 <33016226+Iamlong007@users.noreply.github.com>
2020-11-20 13:33:59 +0100

    responsive

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,59 @@
   <div id="app">
     <div class="bg-white sticky">
       <div class="flex justify-between w-5/6 mx-auto pt-3 pb-3 bg-white">
-        <div>
+        <div class="md:mt-3">
           <img src="./assets/images/logo.svg" alt="Logo Image" />
         </div>
-
-        <button class="lg:hidden" @click="nav = !nav">
+        <div class="mt-2 deskNav">
+          <a href="" class="px-4 hover:text-green-500 hover:font-medium"
+            >Home</a
+          >
+          <a href="" class="px-4 hover:text-green-500 hover:font-medium"
+            >About</a
+          >
+          <a href="" class="px-4 hover:text-green-500 hover:font-medium"
+            >Contact</a
+          >
+          <a href="" class="px-4 hover:text-green-500 hover:font-medium"
+            >Blog</a
+          >
+          <a href="" class="px-4 hover:text-green-500 hover:font-medium"
+            >Career</a
+          >
+        </div>
+        <button
+          class="navBt focus:outline-none focus:shadow-outline bg-gradient-to-r from-primary to-secondary py-3 px-10 rounded-full font-medium text-gray-100 text-sm"
+        >
+          Request Invite
+        </button>
+        <button class="md:hidden" @click="nav = !nav">
           <img :src="toggle" alt="" class="transition" />
         </button>
       </div>
     </div>
 
-    <div class="bg-mobile-background bg-gray-300 bg-no-repeat bg-contain">
-      <div class="w-11/12 mx-auto">
+    <div
+      class="background bg-mobile-background md:bg-desktop-background md:bg-auto bg-gray-300 bg-no-repeat bg-contain"
+    >
+      <div class="w-11/12 mx-auto md:w-12/12 flex-auto md:flex-row pb-16">
+        <div class="md:w-6/12 md:float-right">
+          <img src="./assets/images/image-mockups.png" alt="" class="mock" />
+        </div>
+        <div class="md:w-4/12 md:pt-24 md:pb-32">
+          <h1 class="text-3xl md:text-5xl md:text-left pt-6 font-medium">
+            Next generation digital banking
+          </h1>
+          <p class="text-gray-600 md:text-left pt-4 text-center">
+            Take your financial life online. Your Easy account will be a
+            one-stop-shop for spending, saving, budgeting, investing and much
+            more.
+          </p>
+          <button
+            class="md:float-left mx-auto mt-8 focus:outline-none focus:shadow-outline bg-gradient-to-r from-primary to-secondary py-3 px-10 rounded-full font-medium text-gray-100 text-sm"
+          >
+            Request Invite
+          </button>
+        </div>
         <!-- <transition name="fade">
           <div
             v-if="nav"
@@ -26,22 +67,7 @@
             <p class="p-2">Careers</p>
           </div>
         </transition> -->
-        <div class="w-11/12 mx-auto flex-col">
-          <img src="./assets/images/image-mockups.png" alt="" class="mock" />
-          <h1 class="text-3xl pt-6 font-medium">
-            Next generation digital banking
-          </h1>
-          <p class="text-gray-600 pt-4 text-center">
-            Take your financial life online. Your Easy account will be a
-            one-stop-shop for spending, saving, budgeting, investing and much
-            more.
-          </p>
-          <button
-            class="mx-auto mt-8 focus:outline-none focus:shadow-outline bg-gradient-to-r from-primary to-secondary py-3 px-10 rounded-full font-medium text-gray-100 text-sm"
-          >
-            Request Invite
-          </button>
-        </div>
+        <!-- <div class="w-11/12 mx-auto"></div> -->
       </div>
     </div>
   </div>
@@ -94,5 +120,20 @@ export default {
 }
 .mock {
   margin-top: -30%;
+}
+@media (min-width: 768px) {
+  .background {
+    background-position-x: 35rem !important;
+    background-position-y: -17rem !important;
+  }
+  .mock {
+    margin-top: -17%;
+  }
+}
+@media (max-width: 768px) {
+  .deskNav,
+  .navBt {
+    display: none;
+  }
 }
 </style>
